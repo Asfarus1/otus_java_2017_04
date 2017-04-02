22faf9e3c33e87eff9e7c62d38c946d5cc00cc0c
Asfarus1 <m.a.v.norm@mail.ru>
2017-04-02 20:13:05 +1000

    1st commit

---
+++ hw01/src/main/java/ru/otus_matveev_anton/Main.java
@@ -0,0 +1,10 @@
+package ru.otus_matveev_anton;
+
+/**
+ * Created by asfarus on 02.04.17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("otus hw 01");
+    }
+}
