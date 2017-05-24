296e2db3958a9a54f72b7846edc69539404a9e0c
Asfarus1 <Asfarus1@gmail.com>
2017-05-25 09:02:14 +1000

    hw07 left to do tests

--- hw07/src/main/java/ru/otus_matveev_anton/atm/ATMImpl.java
+++ hw07/src/main/java/ru/otus_matveev_anton/atm/ATMImpl.java
@@ -1,61 +1,54 @@
 package ru.otus_matveev_anton.atm;
 
+import java.util.Arrays;
 import java.util.Collection;
-import java.util.Set;
-import java.util.TreeSet;
 
+//вынес алгоритм снятия денег из Cell в ATM т.к. считаю что ячейка просто просто хранит банкноты а атм решает как их выдавать
 public class ATMImpl implements ATM {
-    private Set<Cell> cells = new TreeSet<>((o1, o2) -> Integer.compare(o2.getNominal(), o1.getNominal()));
+    private final Cell[] cells;
 
     public ATMImpl(Collection<Cell> cells) {
-        this.cells.addAll(cells);
+        this.cells = cells.toArray(new Cell[cells.size()]);
+        Arrays.sort(this.cells, (o1, o2) -> Integer.compare(o2.getNominal(), o1.getNominal()));
     }
 
     @Override
     public int getBalance() {
-        return cells.stream().mapToInt((cell) -> cell.getNominal() * cell.getCount()).reduce(Integer::sum).orElse(0);
+        return Arrays.stream(cells).mapToInt((cell) -> cell.getNominal() * cell.getCount()).reduce(Integer::sum).orElse(0);
     }
 
     @Override
     public void download() {
-        this.cells.forEach(Cell::download);
+        Arrays.stream(cells).forEach(Cell::download);
+    }
+
+    @Override
+    public boolean withdraw(int requred) {
+        return cells.length != 0 && withdrawFromSequenceCells(0,requred);
     }
 
     //ищем первую комбинацию в которой максимально число банкнот большего номинала
     //цикл нужен для случая больший номинал не кратен меньшему, допустим номиналы 5 и 2 при требуемой сумме 6
-    @Override
-    public boolean withdraw(int requred) {
-        if (!cells.isEmpty()) {
-            Cell[] cellArray = cells.toArray(new Cell[cells.size()]);
+    private boolean withdrawFromSequenceCells(int initPos, int requred) {
+        Cell cell = cells[initPos];
+        int nominal = cell.getNominal();
+        int currentNominalCount = Math.min(requred / nominal, cell.getCount());
 
-            class cellSequence {
+        withdrawNextCell:
+        if (currentNominalCount * nominal != requred) {
 
-                private boolean withdraw(int initPos, int requred) {
-                    Cell cell = cellArray[initPos];
-                    int nominal = cell.getNominal();
-                    int currentNominalCount = Math.min(requred / nominal, cell.getCount());
-
-                    withdrawNextCell:
-                    if (currentNominalCount * nominal != requred) {
-
-                        if (initPos < cellArray.length - 1) {
-                            for(;currentNominalCount > -1; currentNominalCount--) {
-                                if (withdraw(initPos + 1, requred - currentNominalCount * nominal)) {
-                                    break withdrawNextCell;
-                                }
-                            }
-                        }
-                        return false;
+            if (initPos < cells.length - 1) {
+                for(;currentNominalCount > -1; currentNominalCount--) {
+                    if (withdrawFromSequenceCells(initPos + 1, requred - currentNominalCount * nominal)) {
+                        break withdrawNextCell;
                     }
-                    if (currentNominalCount != 0) {
-                        cell.withdraw(currentNominalCount);
-                    }
-                    return true;
                 }
             }
-
-            return new cellSequence().withdraw(0, requred);
+            return false;
         }
-        return false;
+        if (currentNominalCount != 0) {
+            cell.withdraw(currentNominalCount);
+        }
+        return true;
     }
 }
