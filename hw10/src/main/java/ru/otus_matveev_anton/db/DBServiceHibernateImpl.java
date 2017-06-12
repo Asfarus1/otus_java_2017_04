7c3ccec96cca088d01518fbfb77cccf5d221170a
Asfarus1 <Asfarus1@gmail.com>
2017-06-13 08:48:43 +1000

    hw10- fix readme.md add abstract DBServiceHibernate

--- hw10/src/main/java/ru/otus_matveev_anton/db/DBServiceHibernateImpl.java
+++ hw10/src/main/java/ru/otus_matveev_anton/db/DBServiceHibernateImpl.java
@@ -1,20 +1,14 @@
 package ru.otus_matveev_anton.db;
 
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
-import ru.otus_matveev_anton.db.dao.UserDaoHibernateImpl;
 import ru.otus_matveev_anton.db.data_sets.AdressDataSet;
 import ru.otus_matveev_anton.db.data_sets.PhoneDataSet;
-import ru.otus_matveev_anton.db.data_sets.UserDataSet;
 import ru.otus_matveev_anton.db.data_sets.UserWithAdressAndPhonesDateSet;
 
-public class DBServiceHibernateImpl implements DBService{
-    private final SessionFactory sessionFactory;
+public class DBServiceHibernateImpl extends DBServiceHibernate {
 
-    public DBServiceHibernateImpl() {
+    @Override
+    public Configuration configure() {
         Configuration cfg = new Configuration();
         cfg.addAnnotatedClass(AdressDataSet.class);
         cfg.addAnnotatedClass(PhoneDataSet.class);
@@ -31,32 +25,6 @@ public class DBServiceHibernateImpl implements DBService{
         cfg.setProperty("hibernate.connection.useSSL", "false");
         cfg.setProperty("hibernate.enable_lazy_load_no_trans", "true");
 
-        sessionFactory = createSessionFactory(cfg);
-    }
-
-    private static SessionFactory createSessionFactory(Configuration configuration) {
-        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder();
-        builder.applySettings(configuration.getProperties());
-        ServiceRegistry serviceRegistry = builder.build();
-        return configuration.buildSessionFactory(serviceRegistry);
-    }
-
-    @Override
-    public void saveUser(UserDataSet user) {
-        try(Session session = sessionFactory.openSession()){
-            new UserDaoHibernateImpl(session).save((UserWithAdressAndPhonesDateSet)user);
-        }
-    }
-
-    @Override
-    public UserWithAdressAndPhonesDateSet getUser(long id) {
-        try(Session session = sessionFactory.openSession()){
-            return new UserDaoHibernateImpl(session).get(id);
-        }
-    }
-
-    @Override
-    public void shutdown() {
-        sessionFactory.close();
+        return cfg;
     }
 }
