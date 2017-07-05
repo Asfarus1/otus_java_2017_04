85feccecbf624cc3d10cd17263f74fcbf19a86da
asfarus1 <Allirodon1>
2017-07-05 22:49:40 +1000

    js refac

--- hw12/public_html/script.js
+++ hw12/public_html/script.js
@@ -1,48 +1,41 @@
-
-function refresh(readonlyfiels)
-{
-    $.ajax({
-
-        type: "POST",
-        url: "cacheData?action=" + (!!readonlyfiels ? 'getreadonly' : 'all'),
-        cache: false,
-        success: function(json){
-            var elem;
-            for(var key in json){
-                elem = $('#' + key);
-                if (elem.attr('type') === 'checkbox'){
-                    elem.prop('checked', json[key]);
-                }else{
-                    elem.val(json[key]);
+function refresh($readonlyfiels) {
+    sendData(
+        "cacheData?action=" + (!!$readonlyfiels ? 'getreadonly' : 'all')
+        , {}
+        , $json => {
+            for (let $key in $json) {
+                let $elem = $('#' + $key);
+                if ($elem.type.is('checkbox')) {
+                    $elem.prop('checked', json[$key]);
+                } else {
+                    $elem.val(json[$key]);
                 }
             }
         }
-    });
+    );
 }
 
-function save()
-{
-    var json = {};
+function save() {
+    let $json = {};
     $('input:not([readonly])').each(
-        function () {
-            if (this.type === 'checkbox'){
-                json[this.id] = this.checked;
-            }else {
-                json[this.id] = this.value;
-            }
-        }
+        $json[this.id] = elem.type.is('checkbox') ? this.checked : this.value
     );
+    sendData("cacheData?action=save", JSON.stringify($json));
+}
+
+function sendData($url, $data, $callback) {
     $.ajax({
-        type: "POST",
-        url: "cacheData?action=save",
+        type: 'POST',
+        url: $url,
         cache: false,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        data:JSON.stringify(json)
+        success: $callback,
+        data: $data
     });
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
     refresh(false);
-    setInterval('refresh(true)',1000);
+    setInterval('refresh(true)', 1000);
 });
