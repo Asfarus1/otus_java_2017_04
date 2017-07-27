623150909484fb587b332784bbe6b03d89dceff2
Asfarus1 <Allirodon1>
2017-07-27 17:52:04 +1000

    pre hw14

---
+++ hw14/src/main/java/ru/otus_matveev_anton/Main.java
@@ -0,0 +1,10 @@
+package ru.otus_matveev_anton;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("ffff");
+    }
+}
